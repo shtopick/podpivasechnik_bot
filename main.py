36b70b2fb34875da165ccdc0c9a6a341053f415a
shtopick <nefartur@mail.ru>
2024-05-07 14:23:54 +0300

    test

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-print("zdarova, zaebal")
+import requests
+from pydantic import BaseModel, Field
 
-print("some changes")
+
+class Coordinates(BaseModel):
+    latitude: float
+    longitude: float
+
+
+class IssRequest(BaseModel):
+    message: str
+    timestamp: int
+    position: Coordinates = Field(alias='iss_position')
+
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(api_url)
+if response.status_code == 200:
+    iss = IssRequest.model_validate_json(response.text)
+    print(iss.position)
+else:
+    print(response.status_code)
