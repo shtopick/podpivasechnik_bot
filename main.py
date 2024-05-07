390a749905fa2161276331eadc635c05096deb85
shtopick <nefartur@mail.ru>
2024-05-07 15:13:17 +0300

    kotiki

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import time
 from pprint import pprint
 
 API_URL = 'https://api.telegram.org/bot'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
 BOT_TOKEN = '6598248184:AAFz9h0Keo_OrNr33nQjWc7eRrteaOjt_Wk'
-TEXT = 'Пиво льется через край'
+ERROR_TEXT = 'Пиво льется через край'
 MAX_COUNTER = 100
 
 offset = -2
 counter = 0
+cat_response: requests.Response
 chat_id: int
 
 
@@ -18,13 +20,18 @@ while counter < MAX_COUNTER:
 
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
-    pprint(updates)
+#    pprint(updates)
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
     time.sleep(1)
     counter += 1
