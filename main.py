40cd423334c930f1d2a6136699f9c5655ad70be3
shtopick <nefartur@mail.ru>
2024-05-14 19:54:19 +0300

    mem

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+import requests
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 BOT_TOKEN = '6598248184:AAFz9h0Keo_OrNr33nQjWc7eRrteaOjt_Wk'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
 
 # Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
@@ -30,6 +32,17 @@ async def process_help_command(message: Message):
 @dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
+
+
+# Этот хэндлер будет срабатывать на команду "/support"
+@dp.message(Command(commands=['support']))
+async def process_help_command(message: Message):
+    await message.answer(
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+        )
 
 
 if __name__ == '__main__':
